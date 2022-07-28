62b56e45c26304feb15a8e4513819553c7000740
kuan <apkapb@gmail.com>
2022-07-28 20:55:09 +0800

    clear: parse_struct

---
+++ pkg/utils/parse_struct.go
@@ -0,0 +1,59 @@
+package utils
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"io/ioutil"
+)
+
+type StructFields []*ast.Field
+
+func ParseStruct(filename string, src []byte) (structMap map[string]StructFields, err error) {
+	structMap = make(map[string]StructFields)
+
+	if src == nil {
+		src, err = ioutil.ReadFile(filename)
+		if err != nil {
+			return structMap, err
+		}
+	}
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, filename, src, parser.ParseComments)
+	if err != nil {
+		return structMap, err
+	}
+
+	collectStructs := func(x ast.Node) bool {
+		ts, ok := x.(*ast.TypeSpec)
+		if !ok || ts.Type == nil {
+			return true
+		}
+
+		// 获取结构体名称
+		structName := ts.Name.Name
+
+		s, ok := ts.Type.(*ast.StructType)
+		if !ok {
+			return true
+		}
+
+		for _, field := range s.Fields.List {
+			//tag := field.Tag.Value
+			//tag = strings.Trim(tag, "`")
+			//tags, err := structtag.Parse(string(tag))
+			//if err != nil {
+			//	return true
+			//}
+			//_, err = tags.Get(tagName)
+			if err == nil {
+				structMap[structName] = append(structMap[structName], field)
+			}
+		}
+		return false
+	}
+
+	ast.Inspect(file, collectStructs)
+
+	return structMap, nil
+}
