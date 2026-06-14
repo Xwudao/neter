d58594ccf0496ca534a9a4ac0eabb6403d307e4a
Tim <apkapb@gmail.com>
2026-06-14 22:17:02 +0800

    feat: refactor process interruption and termination for Windows environment

--- cmd/nr/cmd/dev_process_windows.go
+++ cmd/nr/cmd/dev_process_windows.go
@@ -3,17 +3,35 @@
 package cmd
 
 import (
-	"os"
+	"fmt"
 	"os/exec"
+	"strconv"
 )
 
 func configureDevCommand(cmd *exec.Cmd) {
 }
 
 func interruptDevProcess(cmd *exec.Cmd) error {
-	return cmd.Process.Signal(os.Interrupt)
+	return taskkill(cmd, false)
 }
 
 func killDevProcess(cmd *exec.Cmd) error {
-	return cmd.Process.Kill()
+	return taskkill(cmd, true)
+}
+
+func taskkill(cmd *exec.Cmd, force bool) error {
+	if cmd == nil || cmd.Process == nil {
+		return fmt.Errorf("process not started")
+	}
+
+	args := []string{"/PID", strconv.Itoa(cmd.Process.Pid), "/T"}
+	if force {
+		args = append(args, "/F")
+	}
+
+	killCmd := exec.Command("taskkill", args...)
+	if output, err := killCmd.CombinedOutput(); err != nil {
+		return fmt.Errorf("taskkill %v: %w: %s", args, err, string(output))
+	}
+	return nil
 }
